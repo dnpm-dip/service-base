bec10963e6c7fcf55185faf22bbb21c4ebd1b47c
Lucien Clin <lucien.clin@uni-tuebingen.de>
2023-12-19 15:02:48 +0100

    Renamed a couple of fields

--- src/main/scala/de/dnpm/dip/service/query/ResultSet.scala
+++ src/main/scala/de/dnpm/dip/service/query/ResultSet.scala
@@ -21,7 +21,9 @@ trait ResultSet[PatientRecord,Criteria]
 
   type SummaryType <: ResultSet.Summary
 
+
   val id: Query.Id
+
 
   def summary(
     f: PatientRecord => Boolean = _ => true
@@ -45,29 +47,26 @@ object ResultSet
 
   final case class Demographics
   (
-    gender: Seq[ConceptCount[Coding[Gender.Value]]],
-    age: Seq[ConceptCount[Interval[Int]]],
-    site: Seq[ConceptCount[Coding[Site]]]
+    genderDistribution: Seq[ConceptCount[Coding[Gender.Value]]],
+    ageDistribution: Seq[ConceptCount[Interval[Int]]],
+    siteDistribution: Seq[ConceptCount[Coding[Site]]]
   )
 
   object Demographics extends ReportingOps
   {
-    def on(patients: Seq[Patient]) = {
-
-
+    def on(patients: Seq[Patient]) =
       ResultSet.Demographics(
         DistributionOf(patients.map(_.gender)),
         AgeDistribution(patients.map(_.age)),
         DistributionOf(patients.flatMap(_.managingSite))
       )
-    }
   }
 
 
   trait Summary
   {
     val id: Query.Id
-    val numPatients: Int
+    val patientCount: Int
     val demographics: Demographics
   }
 
@@ -76,108 +75,3 @@ object ResultSet
     Json.writes[Demographics]
 
 }
-
-
-
-/*
-trait ResultSet[PatientRecord,Criteria]
-{
-  self =>
-
-  val id: Query.Id
-
-  def summary(
-    f: PatientRecord => Boolean = _ => true
-  ): ResultSet.Summary
-
-
-  def patientMatches(
-    f: PatientRecord => Boolean = _ => true
-  ): Seq[PatientMatch[Criteria]]
-
-
-  def patientRecord(
-    patId: Id[Patient]
-  ): Option[PatientRecord] 
-
-}
-
-
-object ResultSet
-{
-
-  final case class Demographics
-  (
-    gender: Seq[ConceptCount[Coding[Gender.Value]]],
-    age: Seq[ConceptCount[Interval[Int]]],
-    site: Seq[ConceptCount[Coding[Site]]]
-  )
-
-  final case class Summary
-  (
-    id: Query.Id,
-    numPatients: Int,
-    demographics: Demographics
-  )
-
-
-  implicit val writesDemographics: OWrites[Demographics] =
-    Json.writes[Demographics]
-
-  implicit val writesSummary: OWrites[Summary] =
-    Json.writes[Summary]
-
-}
-
-
-
-trait ResultSet[PatientRecord,Criteria]
-{
-  self =>
-
-  type Summary <: ResultSet.Summary
-
-
-  val id: Query.Id
-
-  def summary(
-    f: PatientRecord => Boolean = _ => true
-  ): Summary
-
-
-  def patientMatches(
-    f: PatientRecord => Boolean = _ => true
-  ): Seq[PatientMatch[Criteria]]
-
-
-  def patientRecord(
-    patId: Id[Patient]
-  ): Option[PatientRecord] 
-
-}
-
-object ResultSet
-{
-
-  final case class Demographics 
-  (
-    gender: Seq[ConceptCount[Coding[Gender.Value]]],
-    age: Seq[ConceptCount[Interval[Int]]],
-    site: Seq[ConceptCount[Coding[Site]]]
-  )
-
-
-  trait Summary
-  {
-
-    val id: Query.Id
-
-    def numPatients: Int
-
-    def demographics: Demographics
-
-  }
-
-}
-*/
-
