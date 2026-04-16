9ad6c34617f0ea5082282641e4ec5c81dcaa5f07
Lucien Clin <lucien.clin@uni-tuebingen.de>
2026-04-16 16:10:02 +0200

    chore: Fixed compilation error

--- src/main/scala/de/dnpm/dip/service/validation/Validators.scala
+++ src/main/scala/de/dnpm/dip/service/validation/Validators.scala
@@ -438,7 +438,7 @@ trait Validators
             .map(_ => d)
         ),
         (metadata.researchConsents.filter(_.nonEmpty) orElse metadata.reasonResearchConsentMissing) must be (defined) otherwise (
-          Error("Es muss entweder MII Forschungs-/Broad-Consent oder der Grund für dessen Fehlen vorhanden sein")
+          Error("Es muss entweder MII Forschungs-/Broad-Consent oder der Grund für dessen Fehlen vorhanden sein") at "Broad Consent"
         ),
         if (today isAfter extendedQcEnforcementDate)
           valueIn (metadata.reasonResearchConsentMissing) must be (in (admissibleConsentMissingReasons)) otherwise (
